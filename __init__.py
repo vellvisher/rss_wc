8ac9cf0f1bde64755834620a6b556446b613a389
v <vaarnan@gmail.com>
2012-09-18 09:26:32 +0800

    Added my blog as default :-)

--- __init__.py
+++ __init__.py
@@ -1,6 +1,8 @@
 import feedparser, sys
 from HTMLParser import HTMLParser
 
+blogUrl = 'http://shiftv.blogspot.com/rss.xml'
+
 class MLStripper(HTMLParser):
     def __init__(self):
         self.reset()
@@ -15,7 +17,10 @@ def stripTags(html):
     s.feed(html)
     return s.get_data()
 
-d = feedparser.parse(sys.argv[1])
+if len(sys.argv) >= 2:
+    blogUrl = sys.argv[1]
+
+d = feedparser.parse(blogUrl)
 wc = 0
 for post in d.entries:
     wc += len(stripTags(post.summary).split())
